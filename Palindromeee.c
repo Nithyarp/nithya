ad54321a41ca1c0002f6a6af3318b07b6f15821e
Nithyarp <30360773+Nithyarp@users.noreply.github.com>
2017-07-25 19:35:05 +0530

    Create Palindromeee.c

---
+++ Palindromeee.c
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+int n,num,digit, reversed=0;
+clrscr();
+printf("\n enter the number:");
+scanf("%d",&num);
+n=num;
+do
+{
+digit=num%10;
+reverse=(reverse*10)+digit;
+num=num/10;
+}while(num!=0);
+printf("\n the reversed number is %d", reverse);
+if(n=reverse)
+printf("\n palindrome number");
+else
+printf("\n not a palindrome number");
+getch();
+}
